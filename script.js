ad9ea443c044b9b55b5ecd6b1c450793f3facd27
[SAYATYA DAS] <[dassayatya@gmail.com]>
2023-04-28 23:28:32 +0530

    java funtion handson 2

---
+++ script.js
@@ -0,0 +1,86 @@
+// DAY 1
+// 1.
+// function myFunction() {
+//     console.log("This is my function!");
+//   }
+//   myFunction(); 
+
+// 2.
+// function addition(Num1,Num2){
+//     document.write(Num1+Num2);
+// }
+// addition(3,4);
+
+// 3.
+//  const multiply = (num1, num2) => num1 * num2;
+
+// 4.
+// var x = 21;
+// var girl = function () {
+//     console.log(x);
+//     var x = 20;
+// };
+// girl ();
+
+// 5.
+// var x = 21;
+//                 girl ();
+//                 console.log(x)
+//                 function girl() {
+//                     console.log(x);
+//                     var x = 20;
+// };
+
+// 6.
+// var x = 21;
+//                 a();
+//                 b();
+                
+//                   function a() {
+                    
+//                    x = 20;
+//                   console.log(x);
+//                 };
+//                  function b() {
+                    
+//                     x = 40;
+//                    console.log(x);
+//                 };
+
+// DAY 2
+
+// 1.
+// function FindSum(a, b){
+//     return a + b;
+// }
+
+// function DisplayData(data, batch){
+//     console.log(`i am from ${data} and My batch is EA${batch}`)
+// }
+
+// DisplayData("PrepBytes", FindSum(10, 9));
+
+// 2.
+// Abc();
+//                 const Abc = function(){
+//                     let value = 20;
+//                     console.log(value);
+//                 }
+
+// 3.
+// var a = 10;
+//                (function (){
+//                    console.log(a);
+//                    var a = 20;
+//                })();
+
+// 4.
+// const greet =  function(name){
+//     return function(m){
+    
+//         console.log(`Hi!! ${name}, ${m}`);
+//     }
+// }
+  
+// const greet_message = greet('EA22');
+// greet_message("Welcome To PrepBytes")
